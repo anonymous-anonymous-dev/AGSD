a44a56f0168c75418135d728ad06e8dadadce347
anonymous-anonymous-dev <179722824+anonymous-anonymous-dev@users.noreply.github.com>
2024-08-29 14:14:23 +1000

    _

--- download_pretrained_models.sh
+++ download_pretrained_models.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 cd $HOME/
-git clone https://github.com/anonymous-anonymous-dev/download_pretrained_models.git
+git clone "https://github.com/anonymous-anonymous-dev/download_pretrained_models.git"
 cd download_pretrained_models
+git remote -v
+git pull origin main
 git remote remove origin
-mv results_agsd_some/ ../__all_results__/p1_hasnets/results_agsd_some/
+mv results_agsd_some/ ../__all_results__/_p1_hasnets/
 cd $HOME/code/AGSD/
